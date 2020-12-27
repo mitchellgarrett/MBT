1ffa59e18ede37cc00506aa8a2fbbca3561641b9
mitchellgarrett <45768600+mitchellgarrett@users.noreply.github.com>
2020-12-26 19:01:00 -0600

    Create Log.cs

---
+++ Example/Log.cs
@@ -0,0 +1,23 @@
+﻿using MBT;
+using UnityEngine;
+
+public class Log : ActionNode {
+
+    string key;
+    string text;
+
+    public Log() : this("log") { }
+
+    public Log(string logKey) {
+        this.key = logKey;
+    }
+
+    public override void Initialize(NodeData data) {
+        base.Initialize(data);
+        text = data.Get<string>(key);
+    }
+
+    protected override BehaviorState ExecuteAction() {
+        return BehaviorState.Success;
+    }
+}
